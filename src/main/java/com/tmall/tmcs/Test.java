b33601120524c0f8ca7aa9e8174114995be9a372
shaokun.ssk <shaokun.ssk@alibaba-inc.com>
2017-01-23 14:57:59 +0800

    2017年1月23日14:57:57

---
+++ src/main/java/com/tmall/tmcs/Test.java
@@ -0,0 +1,19 @@
+package com.tmall.tmcs;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by shaokun.sanjie on 2016/12/27.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("userId",1224);
+        paramsMap.put("aearId",1234);
+        System.out.println(JSON.toJSONString(paramsMap));
+    }
+}
