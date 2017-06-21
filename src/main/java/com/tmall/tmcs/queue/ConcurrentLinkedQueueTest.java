f05d594bd9a67ef3008ddad056c8a2735ed61a34
song5749 <625535361@qq.com>
2017-06-22 00:51:04 +0800

    2017年6月22日00:51:03

---
+++ src/main/java/com/tmall/tmcs/queue/ConcurrentLinkedQueueTest.java
@@ -0,0 +1,20 @@
+package com.tmall.tmcs.queue;
+
+/**
+ * Created by Administrator on 2017/6/22.
+ * ConcurrentLinkedQueue 是适合高并发场景的队列，通过无锁的方式实现高并发下的高性能，
+ * 通常ConcurrentLinkedQueue的性能要好于BlockingQueue。它实现了一个基于链接节点的无界限程安全队列
+ * 该队列遵循了先进先出的原则，头是最先加入的，尾是最后加入的，该队列不允许null元素
+ *
+ * 重要方法
+ *
+ * add()和offer()都是加入元素ConcurrentLinkedQueue 中两个方法无区别
+ * poll()和peek()是取出头元素，区别在于poll会在取出后删除元素 peek则不会被删除
+ *
+ */
+public class ConcurrentLinkedQueueTest {
+
+    public static void main(String[] args) {
+
+    }
+}
