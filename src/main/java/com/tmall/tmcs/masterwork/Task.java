53f90e27311b8b4004b76d5fdf77c4c7573e2aea
song5749 <625535361@qq.com>
2017-12-06 23:49:56 +0800

    masterwork

---
+++ src/main/java/com/tmall/tmcs/masterwork/Task.java
@@ -0,0 +1,43 @@
+package com.tmall.tmcs.masterwork;
+
+/**
+ * Created by Administrator on 2017/12/6.
+ */
+public class Task {
+
+    private int id;
+
+    private String name;
+
+    private int value;
+
+    public Task(int id, String name, int value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
